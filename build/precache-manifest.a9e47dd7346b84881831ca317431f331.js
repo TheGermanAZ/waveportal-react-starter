b1f3b2a58f39e0fafc4b1b05a101d6d61d7e4523
German Alvarez <germanalvarez14@gmail.com>
2021-11-21 21:16:58 -0800

    add build folder

---
+++ build/precache-manifest.a9e47dd7346b84881831ca317431f331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7011726d3b56225f3f1757a854909c16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "133245dcb863d921def3",
+    "url": "/static/css/main.233cbc38.chunk.css"
+  },
+  {
+    "revision": "f1d3d52cb70c6a80652c",
+    "url": "/static/js/2.c526ec28.chunk.js"
+  },
+  {
+    "revision": "133245dcb863d921def3",
+    "url": "/static/js/main.5bc2715b.chunk.js"
+  },
+  {
+    "revision": "bc7615e31f6ee92c18d9",
+    "url": "/static/js/runtime-main.b33eb1b0.js"
+  }
+]);
